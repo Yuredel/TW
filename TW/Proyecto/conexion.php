82b3659cf5de6252c5c67d04e0a5f31e102fbb43
Yurena <ins_pirate@hotmail.com>
2017-05-04 10:02:43 +0200

    desarrollando

--- TW/Proyecto/conexion.php
+++ TW/Proyecto/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = mysql_connect("127.0.0.1", "root", "admin", "proyecto");
+$conexion = mysqli_connect("127.0.0.1", "root", "admin", "proyecto");
    if (!$conexion) {
        die("Conexion Fallida: " . mysqli_connect_error());
    }
